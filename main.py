5336b71badc1169f9730348db80dc6d7e18b2af8
nicolavic98 <nicolavic98@gmail.com>
2017-11-27 22:00:18 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup
+from datetime import datetime
