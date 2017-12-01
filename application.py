523ca3b3f72430328e5db5a68c2cd08892a08c17
nicolavic98 <nicolavic98@gmail.com>
2017-11-30 22:59:19 -0500

    Update application.py

--- application.py
+++ application.py
@@ -11,6 +11,15 @@ from datetime import datetime
 # Configure application
 app = Flask(__name__)
 
+# Ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
