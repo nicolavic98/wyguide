0f114e802b278f9d7e2b32db01b806d1a22c734a
nicolavic98 <nicolavic98@gmail.com>
2017-12-01 01:05:32 -0500

    Update application.py

--- application.py
+++ application.py
@@ -147,8 +147,8 @@ def rank():
     if request.method == "GET":
         return render_template("rank.html", classes=classes, teacher = teacher)
     elif request.method == "POST":
-        teachers = request.form.get("teacher")
-        reviews = db.execute("SELECT teacher FROM class_info INNER JOIN classes ON class_info.name=classes.name WHERE teacher=teachers")
+        teacher = request.form.get("teacher")
+        reviews = db.execute("SELECT teacher FROM class_info INNER JOIN classes ON class_info.name=classes.name WHERE teacher=teacher")
         if not reviews:
             return apology("This teacher and class combo is nonexistent!")
         else:
@@ -164,8 +164,8 @@ def review():
     if request.method == "GET":
         return render_template("review.html", classes=classes, teacher = teacher)
     elif request.method == "POST":
-        teachers = request.form.get("teacher")
-        reviews = db.execute("SELECT teacher FROM class_info INNER JOIN classes ON class_info.name=classes.name WHERE teacher=teachers")
+        teacher = request.form.get("teacher")
+        reviews = db.execute("SELECT teacher FROM class_info INNER JOIN classes ON class_info.name=classes.name WHERE teacher=teacher")
         if not reviews:
             return apology("This teacher and class combo is nonexistent!")
         else:
