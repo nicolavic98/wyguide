3d5b9db342c786569c226ae0a4b3f0eb331783f2
claireburst <31627406+claireburst@users.noreply.github.com>
2017-12-04 14:51:59 -0500

    Update application.py

--- application.py
+++ application.py
@@ -36,6 +36,9 @@ def index():
     new=db.execute("SELECT name, overall FROM teacher_info ORDER BY overall DESC")
     return render_template("index.html", teacher_info = new)
 
+@app.route("/about")
+    return render_template("about.html")
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
